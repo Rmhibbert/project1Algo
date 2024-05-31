8750891608f8edc5aad3ba45556e51452d1c5656
Rory Hibbert <hibbrm1@student.op.ac.nz>
2024-05-31 13:47:02 +1200

    I have added differnt spawn locations and also made the sheep fall off the map

--- 01-starter-code/Assets/RW/Scripts/SheepSpawner.cs
+++ 01-starter-code/Assets/RW/Scripts/SheepSpawner.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using UnityEngine;
+using System.Threading;
+using Random = System.Random;
 
 public class SheepSpawner : MonoBehaviour
 {
     public GameObject sheepPrefab;
-    public float spawnInterval = 3f; // Time between each sheep spawn
-    public Transform spawnPoint; // Far side of the field
-
+    public float spawnInterval = 3f; 
+    public Transform spawnPoint;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
     private float timer = 0f;
+
 
     private void Update()
     {
@@ -24,11 +26,34 @@ public class SheepSpawner : MonoBehaviour
             timer = 0f;
 
             // Spawn a sheep at the spawn point 
-            GameObject newSheep = Instantiate(sheepPrefab, spawnPoint.position, Quaternion.identity);
+            GameObject newSheep = Instantiate(sheepPrefab, locationSelect(), Quaternion.identity);
 
             newSheep.transform.rotation = sheepPrefab.transform.rotation;
 
         }
     }
+
+    private Vector3 locationSelect() 
+    {
+        var location = spawnPoint.position; 
+        Random rand = new Random();
+        int number = rand.Next(1,4);
+
+       switch (number){
+            case 1:
+                location = spawnPoint.position;
+                break;
+            case 2:
+                location = spawnPoint1.position;
+                break;
+            case 3:
+                location = spawnPoint2.position;
+                break;
+        }
+        
+
+        return location;
+    }
+
 }
 
